cf04ba901fd01b848b9c42f8026dce59439ea171
meher92 <meher92@users.noreply.github.com>
2017-04-16 18:13:55 +0530

    restricting the end point to a POST only

--- project/controller.py
+++ project/controller.py
@@ -5,12 +5,12 @@ from tensor_flow_models.tutorials.image.imagenet import classify_image_on_server
 
 from project import app
 
-@app.route('/caption_image', methods=['POST','GET'])
+@app.route('/caption_image', methods=['POST'])
 def caption_image():
     file = request.files['image_file']
     if file:
         response = ci.run_inference_on_image_data(file.read())
     else:
         response = {}
-    print response
+    # print response
     return json.dumps(response)
